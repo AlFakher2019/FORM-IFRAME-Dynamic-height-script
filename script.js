55af52486ec1783d2ca2bd067d561244f9ce1319
Muhammad zain tabasum <zain.tabassum@3li.global>
2025-08-15 14:58:03 +0500

    Add files via upload
    added files

---
+++ script.js
@@ -0,0 +1,10 @@
+
+        var iframe = document.getElementById('iframe');
+
+        window.addEventListener('message', function (event) {
+            if (event.origin === "https://alfakher2019.github.io") {
+                var height = typeof event.data === "number" ? event.data : event.data.iframeHeight;
+                if (height) iframe.style.height = height + 'px';
+            }
+        });
+
